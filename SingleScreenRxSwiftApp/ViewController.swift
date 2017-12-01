480b018764e80ad50e2cc974f9851b8bf1d33927
Alper Akinci <alper.akinci@adastra.one>
2017-12-01 14:40:04 +0100

    Initial Commit

---
+++ SingleScreenRxSwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleScreenRxSwiftApp
+//
+//  Created by Alper Akinci on 01/12/2017.
+//  Copyright © 2017 Alper Akinci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
